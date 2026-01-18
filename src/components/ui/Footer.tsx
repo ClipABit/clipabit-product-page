9bf3a277a0541fdf8d34a747d08cc8896419f783
eshaan-mehta <eshaanm.mehta@gmail.com>
2026-01-18 18:17:22 -0500

    year change

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -8,7 +8,7 @@ function Footer({ }: Props) {
     return (
         <footer className={`flex items-center justify-center p-4 bg-[var(--background)]`}>
             <div className="flex items-center gap-2">
-                <p className={`text-sm text-black dark:text-white`}>© 2025 Clipabit. All rights reserved.</p>
+                <p className={`text-sm text-black dark:text-white`}>© 2026 Clipabit. All rights reserved.</p>
             </div>
         </footer>
     )
